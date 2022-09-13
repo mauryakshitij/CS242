0ab7d0da47cc45adbc0db7b47be17175ebed01bf
mauryakshitij <kkmaurya2004@gmail.com>
2022-09-13 22:45:45 +0530

    assignment 2 first commit

---
+++ Assignment 2/README.md
@@ -0,0 +1,60 @@
+# Assignment 02
+
+---
+
+1. **Name**:  Kshitij Maurya  
+2. **Roll No.** : 210101059
+3. **OS** : Ubuntu 22.04 LTS
+
+## Task 01
+
+- Make a file that contains the list of files to be backed up ( here I have given a list.txt file for the same)
+    - Enter name of each file with extension in new line
+    - Make sure to add a new line after the name of last file
+
+- Create a directory in which files are supposed to be backed up( here I have given a directory named drive for the same)
+
+- Open terminal in the directory containing the script
+- Run the following command to make the script executable
+
+  `$ chmod +x backup.scr`
+
+- Execute the bash script using the following command( change the name of file and directory if you create new file or directory )
+    - the first argument is the name of file and the second argument is the name of directory
+
+    `$ ./backup.scr list.txt drive`
+
+#### Explanation
+
+- The code stores the number of arguments entered and in case it is not equal to two it throws an error accordingly
+- The code checks if the first argument is a file and if the second argument is an existing directory and throws an error if any condition fails
+- A loop reads the name of files from the input file and copies it to the desired directory and adds .bak to its extension
+
+
+## Task 02
+
+- Make a file "input.txt" (already exists in this directory) and enter the string from which new random strings are to be generated in it.
+
+- Open terminal in the directory containing the script
+
+- Run the perl script using the following command 
+    - Enter Count as the first argument and maximum length as the second argument
+
+    `perl random.pl #count #length`
+
+- The program outputs a random string of length less than or equal to the max length
+
+    
+
+#### Explanation
+
+- The code stores the number of arguments entered in a variable and throws an error if the number of arguments entered is not equal to 2.
+
+- If any argument entered is 0, the code throws an error
+
+- Initialise an empty random string and another variable which stores it's length at any instance
+
+- Open "input.txt" and store the string in it as a character array
+
+- A loop runs and generates two random numbers for index and number of repetitions respectively and concatenates the string so generated to the random string to be generated finally 
+
---
+++ Assignment 2/random.pl
@@ -0,0 +1,73 @@
+#!/usr/bin/perl
+use POSIX;
+
+#Storing the number of arguments entered in the terminal in a variable
+my $arguments = $#ARGV+1;
+
+#Throwing an error if the number of arguments entered in the terminal is not equal to 2
+if($arguments != 2){
+    print("Error: Two arguments needed\n");
+    exit();
+}
+
+my $count = $ARGV[0];    #Storing the count argument in a variable
+my $maxLength = $ARGV[1];  #Storing the maximum length argument in a variable
+my $randomString = "";     #Initialising an empty random string
+my $stringLength = 0;      #Initiallising the length of our random string
+
+#Throwing an error if any of the arguments entered is 0
+if($count==0 || $maxLength==0){
+    print("Error: Arguments can not be 0\n");
+    exit();
+}
+
+open(input, "input.txt") or die "File '$input' can't be opened"; #Opening our file from which the base string for generating random string has to be taken
+  
+
+$inputString = <input>;    #Storing the string given in the file in a variable
+chomp($inputString);       #Trimming the edges of input string for unwanted characters
+@baseString = split("", $inputString);  #Splitting the string into characters and storing it in an array of characters
+
+my $lastChar='@'; #Initialising a variable for storing the last randomly chosen character 
+
+#Running a loop for generating our random string
+while(1)
+{    
+
+   my $rand1 = floor(rand(length($inputString)));  #Generating the first random number for selecting the index of character from the base string
+   my $rand2 = floor(rand($count));              #Generating the second random number for the number of times the character is supposed to be repeated
+
+   #Breaking out of the loop if the length of our string shall exceed the maximum length in this iteration
+   if($stringLength+$rand2 > $maxLength){  
+        if($stringLength==0){
+            next;
+        }
+     last;
+   }
+   
+   $stringLength+=$rand2;       #Updating the new length of our random string
+   
+   
+   #Running a loop for generating new random indexes in case the current chosen character is same as the last character
+   while($baseString[$rand1] eq $lastChar){       
+    $rand1 = floor(rand(length($inputString)));
+   }
+   
+   #Updating the value of last character 
+   if($rand2!=0){
+   $lastChar=$baseString[$rand1];
+   }
+
+   #Adding characters to the random string
+   while($rand2--){
+    $randomString=$baseString[$rand1].$randomString;
+   }
+   
+}
+
+#Printing the random string the generated
+print($randomString,"\n");
+
+
+
+   
